799f9a5caf0e7272253c8a9f131c85e099a73be6
Mark Lee <mark@markelee.com>
2019-02-02 16:42:27 -0500

    Pass the savedmodel.zip file and also only download it on ec2 instance

--- src/bash/classifyImagesMarco.sh
+++ src/bash/classifyImagesMarco.sh
@@ -5,14 +5,9 @@ function classifyImagesMarco {
     ## get the needed hostnames
     spark_hosts=($(cat ${PEGASUS_HOME}/tmp/crystal-project-spark-cluster/public_dns))
     postgresql_hosts=($(cat ${PEGASUS_HOME}/tmp/crystal-project-database-cluster/public_dns))
-    ## getting the marco saved model
-    echo "Getting marco trained model..."
-    wget https://storage.googleapis.com/marco-168219-model/savedmodel.zip
     ## copy the pyspark python script
     echo "Copying python script to spark master..."
     scp src/python/classifyImagesMarco.py ubuntu@${spark_hosts[0]}:
-    echo "Copying marco saved model to spark master..."
-    scp savedmodel.zip ubuntu@${spark_hosts[0]}:
 
     ## launch the command
     echo "Running spark classification job..."
@@ -21,6 +16,7 @@ function classifyImagesMarco {
         export LD_LIBRARY_PATH+=:/usr/local/hadoop/lib/native
         export SPARK_HOME=/usr/local/spark
         export HADOOP_HOME=/usr/local/hadoop
-        spark-submit --packages org.apache.hadoop:hadoop-aws:2.7.1,databricks:spark-deep-learning:0.3.0-spark2.2-s_2.11,org.postgresql:postgresql:42.1.4 --master spark://${spark_hosts[0]}:7077 --executor-memory 10G --driver-memory 40G --files saved_model.pb --py-files classifyImagesMarco.py classifyImagesMarco.py $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_DEFAULT_REGION ${postgresql_hosts[0]} $POSTGRES_USER $POSTGRES_PASSWORD
+        wget https://storage.googleapis.com/marco-168219-model/savedmodel.zip
+        spark-submit --packages org.apache.hadoop:hadoop-aws:2.7.1,databricks:spark-deep-learning:0.3.0-spark2.2-s_2.11,org.postgresql:postgresql:42.1.4 --master spark://${spark_hosts[0]}:7077 --executor-memory 10G --driver-memory 40G --files savedmodel.zip --py-files classifyImagesMarco.py classifyImagesMarco.py $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_DEFAULT_REGION ${postgresql_hosts[0]} $POSTGRES_USER $POSTGRES_PASSWORD
         "
 }
